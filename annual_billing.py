d92aa473319697795cd40c27d8927f07b590c858
Claudinei Gomes <62800553+claudineigms@users.noreply.github.com>
2023-07-06 21:44:57 -0300

    Insert anuual billing question

--- annual_billing.py
+++ annual_billing.py
@@ -1,15 +1,3 @@
-def fib(n):
-    return (n if n in [0,1] else fib(n-1) + fib(n-2))
-
-def find_fib(number_find):
-     fib_sequence = []
-     fib_index = 0
-     while fib(fib_index) <= number_find:
-             fib_sequence.append(fib(fib_index))
-             fib_index += 1
-     if number_find in fib_sequence:
-             return True
-     return False
 daily_billing_report = {
     "01/01/2023":100450,
     "15/01/2023":102351,
